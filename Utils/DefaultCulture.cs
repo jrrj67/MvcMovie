ac7163a2a8d321fa2807aa666c07570581ec5791
jrrj67 <ricardojunior.6767@gmail.com>
2021-07-25 19:59:51 -0300

    Add project files.

---
+++ Utils/DefaultCulture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Utils
+{
+    public class DefaultCulture
+    {
+        public static void SetDefaultCulture() 
+        {
+            var cultureInfo = new CultureInfo("en-US");
+            cultureInfo.NumberFormat.CurrencySymbol = "$";
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+        }
+    }
+}
